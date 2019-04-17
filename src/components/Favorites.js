bdc80b5c4175db6143f513484ef02e35d63ca2f2
Nico <nicovigil1@gmail.com>
2019-04-17 08:18:13 -0600

    minor fixes for cors issues and favorites deletion

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -4,18 +4,20 @@ export default class Body extends React.Component {
   deleteFavorite = () => {
     let currentUser = this.props.currentUser
     fetch(`https://hidden-mountain-93492.herokuapp.com/api/v1/favorites?user_id=${currentUser.id}`, {method: "DELETE"})
-      .then(res => {res.json()}) 
+      .then(res => {res.json()})
       .then(res => console.log(res))
   }
   render() {
     return (
       this.props.favorites.data.map(favorite => {
         return <div>
-          {/* <button onClick={this.deleteFavorite}>Delete Favorite</button> */}
-          <p>population: {favorite.attributes.population}</p>
-          <p>number of cyclists: {favorite.attributes.cyclists}</p>
-          <p>number of walkers: {favorite.attributes.walkers}</p>
-          <p>Average Property Value: {favorite.attributes.property_val}</p>
+          {console.log(favorite)}
+          <button onClick={this.deleteFavorite}>Delete Favorite</button>
+          <p>city: {favorite.attributes.city}</p>
+          <p>population: {favorite.attributes.population} people</p>
+          <p>number of cyclists: {favorite.attributes.cyclists} people</p>
+          <p>number of walkers: {favorite.attributes.walkers} people</p>
+          <p>Average Property Value: $ {favorite.attributes.property_val}</p>
         </div>
       })
     )
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,25 +3,26 @@ import { format } from 'path';
 
 export default class SearchResults extends React.Component {
   addFavorite = () => {
+    let city = this.props.city 
     let currentUser = this.props.currentUser
     let favorite = this.props.results.data.attributes
-    let formatFavorite = `user_id=${currentUser.id}&city_id=${favorite.id}&cyclists=${favorite.cyclists}&walkers=${favorite.walkers}&property_val=${favorite.property_val}&population=${favorite.population}`
+    let formatFavorite = `city=${city}&user_id=${currentUser.id}&city_id=${favorite.id}&cyclists=${favorite.cyclists}&walkers=${favorite.walkers}&property_val=${favorite.property_val}&population=${favorite.population}`
     fetch(`https://hidden-mountain-93492.herokuapp.com/api/v1/favorites?${formatFavorite}`, {method: "POST"})
       .then(res => res.json())
       .then(res => console.log(res))
       .catch(err => console.log(err))
   }
+
   render() {
     return( 
       <div>
         {this.props.currentUser && <button onClick={this.addFavorite}>Add favorite!</button>}
         {console.log(this.props.currentUser)}
         {console.log(this.props.results.data.attributes)}
-        <p>population: {this.props.results.data.attributes.population}</p>
-        <p>number of cyclists: {this.props.results.data.attributes.cyclists}</p>
-        <p>number of walkers: {this.props.results.data.attributes.walkers}</p>
-        <p>Average Property Value: {this.props.results.data.attributes.property_val}</p>
-        <p>Average Property Value: {this.props.results.data.attributes.property_val}</p>
+        <p>population: {this.props.results.data.attributes.population} people</p>
+        <p>number of cyclists: {this.props.results.data.attributes.cyclists} people</p>
+        <p>number of walkers: {this.props.results.data.attributes.walkers} people</p>
+        <p>Average Property Value: $ {this.props.results.data.attributes.property_val}</p>
       </div>
     )
   }
